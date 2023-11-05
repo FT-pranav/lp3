0a1ec49014f9ac97adbf161d4cc11b0678cffddf
FT-pranav <149650008+FT-pranav@users.noreply.github.com>
2023-11-05 20:23:36 +0530

    Update fractional_knaosack.py

--- fractional_knaosack.py
+++ fractional_knaosack.py
@@ -1,27 +1,25 @@
+class Item:
+  def __init__(self, profit, weight):
+    self.profit = profit
+    self.weight = weight
 
+  def fractionalKnapsack(W, arr):
+    arr.sort(key=lambda x: (x.profit/x.weight), reverse=True)
+    finalvalue = 0.0
+ 
 
+    for item in arr:
+      if item.weight <= W:
+        W -= item.weight
+        finalvalue += item.profit
+      else:
+        finalvalue += item.profit * W / item.weight
+        break
  
- def fractional_knapsack():
-    weights=[10,20,30]
-    values=[60,100,120]
-    capacity=50
-    res=0
-    # Pair : [Weight,value]
-    for pair in sorted(zip(weights,values), key= lambda x: x[1]/x[0], reverse=True):
-        if capacity<=0: # Capacity completed - Bag fully filled 
-            break 
-        if pair[0]>capacity: # Current's weight with highest value/weight ratio Available Capacity
-            res+=int(capacity * (pair[1]/pair[0]))  # Completely fill the bag
-            capacity=0
-        elif pair[0]<=capacity: # Take the whole object
-            res+=pair[1]
-            capacity-=pair[0]
-    print(res)        
+    return finalvalue
 
-if __name__=="__main__":
-    fractional_knapsack()
-
- 
- 
- 
- 
+  if __name__ == "__main__":
+    W = 50
+    arr = [Item(60, 10), Item(100, 20), Item(120, 30)]
+    max_val = fractionalKnapsack(W,arr)
+    print("Maximum value obtained is : ",max_val)
