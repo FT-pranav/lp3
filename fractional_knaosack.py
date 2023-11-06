278a9c4cbf61e1e9812e3ea3a3dd44c9385c3ff4
FT-pranav <149650008+FT-pranav@users.noreply.github.com>
2023-11-06 07:50:21 +0530

    Update fractional_knaosack.py

--- fractional_knaosack.py
+++ fractional_knaosack.py
@@ -1,25 +1,50 @@
+# Structure for an item which stores weight and
+# corresponding value of Item
 class Item:
-  def __init__(self, profit, weight):
-    self.profit = profit
-    self.weight = weight
+    def __init__(self, profit, weight):
+        self.profit = profit
+        self.weight = weight
 
-  def fractionalKnapsack(W, arr):
+# Main greedy function to solve problem
+
+
+def fractionalKnapsack(W, arr):
+
+    # Sorting Item on basis of ratio
     arr.sort(key=lambda x: (x.profit/x.weight), reverse=True)
-    finalvalue = 0.0
- 
 
+    # Result(value in Knapsack)
+    finalvalue = 0.0
+
+    # Looping through all Items
     for item in arr:
-      if item.weight <= W:
-        W -= item.weight
-        finalvalue += item.profit
-      else:
-        finalvalue += item.profit * W / item.weight
-        break
- 
+
+        # If adding Item won't overflow,
+        # add it completely
+        if item.weight <= W:
+            W -= item.weight
+            finalvalue += item.profit
+
+        # If we can't add current Item,
+        # add fractional part of it
+        else:
+            finalvalue += item.profit * W / item.weight
+            break
+
+    # Returning final value
     return finalvalue
 
-  if __name__ == "__main__":
+
+# Driver Code
+if __name__ == "__main__":
     W = 50
     arr = [Item(60, 10), Item(100, 20), Item(120, 30)]
-    max_val = fractionalKnapsack(W,arr)
-    print("Maximum value obtained is : ",max_val)
+
+    # Function call
+    max_val = fractionalKnapsack(W, arr)
+    print(max_val)
+
+# TC- > O(NlogN)   FOR SORTING
+# SC -> O(N) as no extra space is
+
+# OBJECTIVE IS THAT PROFIT SHOULD BE MAXXIMUM
